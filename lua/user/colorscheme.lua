326dad7e7c6007ca3173a3eb22d53ab074e173dc
TsuiWide <xukuan89@163.com>
2023-06-02 08:08:22 +0000

    commit id

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -15,7 +15,7 @@
 
 local M = {
     "ellisonleao/gruvbox.nvim",
-    --commit = "df149bccb19a02c5c2b9fa6ec0716f0c0487feb0",
+    commit = "df149bccb19a02c5c2b9fa6ec0716f0c0487feb0",
     lazy = false,    -- make sure we load this during startup if it is your main colorscheme
     riority = 1000, -- make sure to load this before all the other start plugins
 }
