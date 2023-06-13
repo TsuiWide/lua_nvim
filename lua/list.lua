89f9ed5d857cb2b29701f051d73b7b2a024c8e66
Tsui Wide <xukuan89@163.com>
2023-06-13 21:10:42 +0800

    add autopairs add nvim cmp

--- lua/list.lua
+++ lua/list.lua
@@ -28,5 +28,31 @@ return {
         "ellisonleao/gruvbox.nvim",
         lazy = false,
         riority = 1000,
-    }
+    },
+    -- 括号自动补全
+    {
+        "windwp/nvim-autopairs",
+        config = function()
+            require("user.nvim-autopairs")
+        end,
+    },
+    -- 自动补全
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            { "onsails/lspkind-nvim" }, -- 为补全添加类似 vscode 的图标
+            { "hrsh7th/vim-vsnip" }, -- vsnip 引擎，用于获得代码片段支持
+            { "hrsh7th/cmp-vsnip" }, -- 适用于 vsnip 的代码片段源
+            { "hrsh7th/cmp-nvim-lsp" }, -- 替换内置 omnifunc，获得更多补全
+            { "hrsh7th/cmp-path" }, -- 路径补全
+            { "hrsh7th/cmp-buffer" }, -- 缓冲区补全
+            { "hrsh7th/cmp-cmdline" }, -- 命令补全
+            { "f3fora/cmp-spell" }, -- 拼写建议
+            { "rafamadriz/friendly-snippets" }, -- 提供多种语言的代码片段
+            { "lukas-reineke/cmp-under-comparator" }, -- 让补全结果的排序更加智能}}
+        },
+        config = function()
+            require("user.nvim-cmp")
+        end,
+    },
 }
