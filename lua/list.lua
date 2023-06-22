b378aea9013a93d1ca84eeefccd348ed9400952c
TsuiWide <xukuan89@163.com>
2023-06-22 17:20:58 +0800

    add lsp zero

--- lua/list.lua
+++ lua/list.lua
@@ -142,4 +142,31 @@ return {
             require("user.neotest")
         end,
     },
+    
+    -- lsp config
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        dependencies = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},
+            {
+                'williamboman/mason.nvim',
+                build = function()
+                    pcall(vim.cmd, 'MasonUpdate')
+                end,
+            },
+            {'williamboman/mason-lspconfig.nvim'},
+
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},
+            {'hrsh7th/cmp-nvim-lsp'},
+            {'L3MON4D3/LuaSnip'},
+        },
+        config = function()
+            require("user.lsp")
+        end,
+    },
+
+    -- end
 }
